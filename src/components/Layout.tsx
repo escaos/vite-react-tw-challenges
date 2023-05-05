ce9047d0f4cb5937e54a514752cd0490a105f2fd
Edison Sanchez <edison@edisonsanchez.com>
2023-05-04 23:40:45 -0400

    Fixing the show me the code button

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,11 +9,11 @@ export const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="w-full h-full flex flex-col justify-center items-center relative">
       <a
-        className="github-button flex flex-row justify-center items-center gap-2 text-[#FFF]"
+        className="github-button flex flex-row justify-center items-center gap-2 text-[#FFF] absolute md:fixed"
         href="https://github.com/escaos/vite-react-tw-challenges"
       >
         <GithubOutlined className="text-[#FFF]" />
-        Show me the code
+        <span>Show me the code</span>
       </a>
 
       <main>{children}</main>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,9 @@ import { FC } from 'react';
 const Home: FC = () => {
   return (
     <>
-      <h1 className="text-center text-xl md:text-5xl p-3 md:p-5">Samples Vite + React + TailwindCSS</h1>
+      <h1 className="text-center text-xl md:text-5xl px-8 md:p-5 pt-[4rem] md:pt-5 ">
+        Samples Vite + React + TailwindCSS
+      </h1>
       <Challenges />
     </>
   );
