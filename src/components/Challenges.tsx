a28b9462de90cef3e842966ec66e8bf818a969c8
Edison Sanchez <edison@edisonsanchez.com>
2023-05-05 00:12:08 -0400

    Adding key

--- src/components/Challenges.tsx
+++ src/components/Challenges.tsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 export const Challenges = () => {
   return (
     <nav className="w-[100%] px-[5%] flex flex-row flex-wrap justify-center gap-[2rem] mt-[2rem]">
-      {CHALLENGES.map((challenge) => {
+      {CHALLENGES.map((challenge, index) => {
         return (
-          <Link className="glass-card w-[16rem] h-[16rem]" to={challenge.url}>
+          <Link key={index} className="glass-card w-[16rem] h-[16rem]" to={challenge.url}>
             <div className="circle" />
             <h2 className="text-2xl md:text-xl mt-2">{challenge.title}</h2>
           </Link>
